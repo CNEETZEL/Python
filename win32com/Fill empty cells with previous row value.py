ee2ad7cd6fc1e9a0cf09ea987fade545c802ac22
CNEETZEL <98472681+CNEETZEL@users.noreply.github.com>
2022-06-20 14:01:15 -0300

    Update Fill empty cells with previous row value.py

--- win32com/Fill empty cells with previous row value.py
+++ win32com/Fill empty cells with previous row value.py
@@ -8,11 +8,17 @@ excel = win32.gencache.EnsureDispatch('Excel.Application')
 wb = excel.Workbooks.Open(fname)
 ws = wb.ActiveSheet
 
-begrow = 2
-endrow = ws.UsedRange.Rows.Count
-for row in range(begrow,endrow+1): 
+#begrow = 2
+#endrow = ws.UsedRange.Rows.Count
+#for row in range(begrow,endrow+1): 
+#  if ws.Range('A{}'.format(row)).Value is None:
+#    ws.Range('A{}'.format(row)).Value = excel.ActiveCell.Offset(2,1).Value
+    
+Start_Row = 2
+End_Row = ws.UsedRange.Rows.Count
+for row in range(Start_Row,End_Row+1):
   if ws.Range('A{}'.format(row)).Value is None:
-    ws.Range('A{}'.format(row)).Value = excel.ActiveCell.Offset(2,1).Value
+    ws.Range('A{}'.format(row)).Value = ws.Range('A{}'.format(row-1)).Value
 
 wb.SaveAs(fname.rsplit('.', 1)[0]+".xlsx", FileFormat:=61)
 wb.Close()
